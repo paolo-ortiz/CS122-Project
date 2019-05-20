99629e8d88c64a97f7cc28d2e1d0658272470870
juliennedc <julieannedelacruz@yahoo.com>
2019-05-20 16:52:12 +0800

    Fixed formatting for INPUT-2-SCREEN

---
+++ templates/add_class.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Output1</title>
+    <!--Import Google Icon Font-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <style type="text/css">
+        .brand {
+            background: #cbb09c !important;
+        }
+
+        .brand-text {
+            color: #cbb09c !important;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+    <body class="grey lighten-4">
+        <!--BACKGROUND-->
+        <nav class="white z-depth-0">
+            <div class="container">
+                <a href="#" class="brand-logo brand-text">Add A Class</a>
+            </div>
+        </nav> 
+
+</html>
