9fb83c2334b635f41c4317ff0a82a4aabccfeac0
juliennedc <julieannedelacruz@yahoo.com>
2019-05-20 18:06:17 +0800

    Update add_class.php

--- add_class.php
+++ add_class.php
@@ -29,7 +29,7 @@
                 <a href="#" class="brand-logo brand-text">Add A Class</a>
             </div>
         </nav>
-        <form action="Input1 header.php" method=POST>
+        <form action="Input2 header.php" method=POST>
             <!-- <input type=text placeholder="Class" name= tclass> -->
             <div class="container" style="text-align:left">
                 <label>Class</label>
@@ -40,15 +40,16 @@
                     <option name="tclass" value=PE>Physical Education</option>
                     <option name="tclass" value=English>English</option>
                 </select>
+                &nbsp;
                 <label>Section</label>
                 <select name=tsec class="browser-default">
-                    <option value="tsec" disabled selected>Choose your option</option>
+                    <option value=tsec disabled selected>Choose your option</option>
                     <option value=A name="tsec">A</option>
                     <option value=B name="tsec">B</option>
                     <option value=C name="tsec">C</option>
                     <option value=D name="tsec">D</option>
                 </select>
-                &nbsp;
+                <br>
                 <button class="btn brand z-depth-0" type="submit" name="action">Add Class
             </button>
             </div>
