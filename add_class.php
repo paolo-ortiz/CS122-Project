52872bc7a88a7548b0062ee25a8deb2bc5c1b939
John Villaos <john.villaos@obf.ateneo.edu>
2019-05-21 22:52:05 +0800

    Designed add_class

--- add_class.php
+++ add_class.php
@@ -1,58 +1,58 @@
 <!DOCTYPE html>
-<html>
 
 <head>
-    <title>Output1</title>
-    <!--Import Google Icon Font-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>OUTPUT 1</title>
+    <link rel="stylesheet" href="css\bootstrap.min.css">
 
     <style type="text/css">
         .brand {
-            background: #cbb09c !important;
+            background: #29b6f6 !important;
         }
 
         .brand-text {
-            color: #cbb09c !important;
+            color: #29b6f6 !important;
         }
     </style>
 </head>
 
-<body>
-
-    <!--JavaScript at end of body for optimized loading-->
-    <script type="text/javascript" src="js/materialize.min.js"></script>
-
-    <body class="grey lighten-4">
-        <!--BACKGROUND-->
-        <nav class="white z-depth-0">
-            <div class="container">
-                <a href="#" class="brand-logo brand-text">Add A Class</a>
-            </div>
-        </nav>
-        <form action="Input2 header.php" method=POST>
-            <!-- <input type=text placeholder="Class" name= tclass> -->
-            <div class="container" style="text-align:left">
-                <label>Class</label>
-                <select name=tclass class="browser-default">
-                    <option value="" disabled selected>Choose your option</option>
-                    <option name="tclass" value=Science>Science</option>
-                    <option name="tclass" value=Math>Mathematics</option>
-                    <option name="tclass" value=PE>Physical Education</option>
-                    <option name="tclass" value=English>English</option>
-                </select>
-                &nbsp;
-                <label>Section</label>
-                <select name=tsec class="browser-default">
-                    <option value=tsec disabled selected>Choose your option</option>
-                    <option value=A name="tsec">A</option>
-                    <option value=B name="tsec">B</option>
-                    <option value=C name="tsec">C</option>
-                    <option value=D name="tsec">D</option>
-                </select>
-                <br>
-                <button class="btn brand z-depth-0" type="submit" name="action">Add Class
-            </button>
-            </div>
+<body class="blue">
+    <!--BACKGROUND-->
+    <nav class="navbar navbar-light" style="background-color:#333D80; color:#B3BDFF; font-face: 16px-apple-system">
+        <a class="navbar-brand" href="#" style="color:#B3BDFF;">
+            <strong>Add a Class</strong>
+        </a>
+        <form action=input1.html method=POST>
+            <button class="btn btn-outline-secondary my-2 my-sm-0" style="border-color:#B3BDFF; color:#B3BDFF;" type="submit"> Log Out</button>
         </form>
-
-</html>
+    </nav>
+    <br>
+    <div class="container-fluid">
+		<div class="row">
+    <form action="Input2 header.php" method=POST>
+        <!-- <input type=text placeholder="Class" name= tclass> -->
+        <div class="container" style="text-align:left">
+            <label>Class</label>
+            <select name=tclass class="browser-default">
+                <option value="" disabled selected>Choose your option</option>
+                <option name="tclass" value=Science>Science</option>
+                <option name="tclass" value=Math>Mathematics</option>
+                <option name="tclass" value=PE>Physical Education</option>
+                <option name="tclass" value=English>English</option>
+            </select>
+            &nbsp;
+            <label>Section</label>
+            <select name=tsec class="browser-default">
+                <option value=tsec disabled selected>Choose your option</option>
+                <option value=A name="tsec">A</option>
+                <option value=B name="tsec">B</option>
+                <option value=C name="tsec">C</option>
+                <option value=D name="tsec">D</option>
+            </select>
+            <br>
+            <button class="btn brand z-depth-0" type="submit" name="action">Add Class
+            </button>
+        </div>
+    </form>
+    </div>
+    </div>
+</body>
