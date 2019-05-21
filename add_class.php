58147737fb8b2428f6eed6dab08cd419dbdb6e3c
John Villaos <john.villaos@obf.ateneo.edu>
2019-05-21 22:54:40 +0800

    Designed add_class

--- add_class.php
+++ add_class.php
@@ -27,32 +27,34 @@
     </nav>
     <br>
     <div class="container-fluid">
-		<div class="row">
-    <form action="Input2 header.php" method=POST>
-        <!-- <input type=text placeholder="Class" name= tclass> -->
-        <div class="container" style="text-align:left">
-            <label>Class</label>
-            <select name=tclass class="browser-default">
-                <option value="" disabled selected>Choose your option</option>
-                <option name="tclass" value=Science>Science</option>
-                <option name="tclass" value=Math>Mathematics</option>
-                <option name="tclass" value=PE>Physical Education</option>
-                <option name="tclass" value=English>English</option>
-            </select>
-            &nbsp;
-            <label>Section</label>
-            <select name=tsec class="browser-default">
-                <option value=tsec disabled selected>Choose your option</option>
-                <option value=A name="tsec">A</option>
-                <option value=B name="tsec">B</option>
-                <option value=C name="tsec">C</option>
-                <option value=D name="tsec">D</option>
-            </select>
-            <br>
-            <button class="btn brand z-depth-0" type="submit" name="action">Add Class
-            </button>
+        <div class="row">
+            <form action="Input2 header.php" method=POST>
+                <!-- <input type=text placeholder="Class" name= tclass> -->
+                <div class="container" style="text-align:left">
+                    <label>Class</label>
+                    <select name=tclass class="browser-default">
+                        <option value="" disabled selected>Choose your option</option>
+                        <option name="tclass" value=Science>Science</option>
+                        <option name="tclass" value=Math>Mathematics</option>
+                        <option name="tclass" value=PE>Physical Education</option>
+                        <option name="tclass" value=English>English</option>
+                    </select>
+                    &nbsp;
+                    <label>Section</label>
+                    <select name=tsec class="browser-default">
+                        <option value=tsec disabled selected>Choose your option</option>
+                        <option value=A name="tsec">A</option>
+                        <option value=B name="tsec">B</option>
+                        <option value=C name="tsec">C</option>
+                        <option value=D name="tsec">D</option>
+                    </select>
+                    <br>
+                    <form action=index.php method=POST>
+                        <button class="btn brand z-depth-0" type="submit" name="action">Add Class
+                        </button>
+                    </form>
+                </div>
+            </form>
         </div>
-    </form>
-    </div>
     </div>
 </body>
