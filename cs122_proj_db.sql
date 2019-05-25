abd57395c02dfb5856e586bb53e730b101da9ebd
Mjust-1124 <45148398+Mjust-1124@users.noreply.github.com>
2019-05-25 12:32:19 +0800

    Add files via upload

--- cs122_proj_db.sql
+++ cs122_proj_db.sql
@@ -1,3 +1,4 @@
+DROP Database cs122_proj;
 CREATE Database cs122_proj; 
 
 CREATE TABLE Teacher (TeacherID int, Primary Key (TeacherID));
@@ -8,20 +9,20 @@ CREATE TABLE Student(StudentID int, SectionName varchar(255), PRIMARY KEY(Studen
 		FOREIGN KEY(SectionName) References Section(SectionName));
 
 CREATE TABLE Teacher_subject (TeacherID int, SubjectID int, PRIMARY KEY(TeacherID, SubjectID), 
-		FOREIGN KEY(TeacherID) References Teacher(TeacherID), FOREIGN KEY SubjectID REFERENCES Subject(SubjectID) );
+		FOREIGN KEY(TeacherID) References Teacher(TeacherID), FOREIGN KEY (SubjectID) REFERENCES Subject(SubjectID) );
 
 CREATE TABLE Requirement(RequirementID varchar(255), TeacherID int, SubjectID int,  PRIMARY KEY(RequirementID), 
 		FOREIGN KEY(TeacherID) References Teacher_subject(TeacherID), 
-		FOREIGN KEY (SubjectID) REFERENCES Teacher_subject(SubjectID);
+		FOREIGN KEY (SubjectID) REFERENCES Teacher_subject(SubjectID));
 
 CREATE TABLE Handled_section(TeacherID int, SubjectID int, SectionName varchar(255),  PRIMARY KEY(TeacherID, SubjectID, SectionName), 
 		FOREIGN KEY(TeacherID) References Teacher_subject(TeacherID), 
 		FOREIGN KEY (SubjectID) REFERENCES Teacher_subject(SubjectID), 
 		FOREIGN KEY (SectionName) REFERENCES Section(SectionName));
 
-CREATE TABLE Grade(StudentID int, RequirementID varchar(255), Grade int,  PRIMARY KEY(StudentID, RequirementID), 
-		FOREIGN KEY(StudentID) References Student(TeacherID), 
-		FOREIGN KEY (RequirementID) REFERENCES Requirement(SectionName));
+CREATE TABLE Grade(StudentID int, RequirementID varchar(255), Grade int, HPS int,  PRIMARY KEY(StudentID, RequirementID), 
+		FOREIGN KEY(StudentID) References Student(StudentID), 
+		FOREIGN KEY (RequirementID) REFERENCES Requirement(RequirementID));
 
 CREATE TABLE Requirement_Percentage(SubjectID int, RequirementID varchar(255), Percentage int, PRIMARY KEY(SubjectID, RequirementID), 
 		FOREIGN KEY (SubjectID) REFERENCES Teacher_subject(SubjectID), 
